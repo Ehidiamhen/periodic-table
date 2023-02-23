df73eff0be69339eb36185d829d1c105a1da6cdb
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2023-02-23 16:36:55 +0100

    Updated table UI

--- src/PeriodicTable/PeriodicTable.jsx
+++ src/PeriodicTable/PeriodicTable.jsx
@@ -3,14 +3,21 @@ import data from "../PeriodicTableJSON.json";
 import "./PeriodicTable.css";
 
 const colorMap = {
-    "noble gas": "#FFBC42",
-    "alkaline earth metal": "#EC674E",
-    "diatomic nonmetal": "#D81159",
-    "alkali metal": "#8F2D56",
-    "transition metal": "#58586B",
-    "post-transition metal": "#218380",
-    "lanthanide": "#4AABAF",
-    "metalloid": "#73D2DE",
+    "noble gas": "#9328fe",
+    "unknown, predicted to be noble gas": "#9328fe",
+    "alkaline earth metal": "#ffa589",
+    "diatomic nonmetal": "#487bde",
+    "polyatomic nonmetal": "#2d6e96",
+    "alkali metal": "#ff4343",
+    "unknown, but predicted to be an alkali metal": "#ff4343",
+    "transition metal": "#ff8a00",
+    "unknown, probably transition metal": "#ff8a00",
+    "post-transition metal": "#31b870",
+    "unknown, probably post-transition metal": "#31b870",
+    "lanthanide": "#699797",
+    "actinide": "#976995",
+    "metalloid": "#1b99c1",
+    "unknown, probably metalloid": "#1b99c1",
 };
 
 export default function PeriodicTable() {
@@ -20,7 +27,7 @@ export default function PeriodicTable() {
         <div
           className={`elements ${element.name}`}
           key={element.name}
-          style={{ gridColumn: element.xpos, gridRow: element.ypos, borderColor: colorMap[element.category], }}
+          style={{ gridColumn: element.xpos, gridRow: element.ypos, borderColor: colorMap[element.category], backgroundColor: colorMap[element.category], }}
         >
           <strong className="symbol">{element.symbol}</strong>
           <small className="number">{element.number}</small>
