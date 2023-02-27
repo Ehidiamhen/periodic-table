7ce9484cc007091fd13bf2481a659b21942a6cea
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2023-02-27 01:34:10 +0100

    completed modal UI and rendered more data

--- src/ElementModal/ElementModal.jsx
+++ src/ElementModal/ElementModal.jsx
@@ -1,15 +1,68 @@
 import React from "react";
-import './ElementModal.css'
+import "./ElementModal.css";
+import { colorMap } from "../PeriodicTable/PeriodicTable";
 
-export default function ElementModal({element, onClose}) {
-    return (
-        <div className="modal-wrap">
-                <div className="element-modal">
-                    <span onClick={onClose}>X</span>
-                    <strong>{element.name}</strong>
-                    <p>{element.summary}</p>
-                    <p>{element.number}</p>
-                </div>
-            </div>
-    )
+export default function ElementModal({ element, onClose }) {
+  return (
+    <div className="modal-wrap">
+      <div
+        style={{ backgroundColor: colorMap[element.category] }}
+        className="element-modal"
+      >
+        <strong>{element.name}</strong>
+        <span className="modal-close" onClick={onClose}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+            width="36px"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </span>
+
+        <p>
+          <span>Atomic Number: </span>
+          {element.number}
+        </p>
+        <p>
+          <span>Atomic Mass: </span>
+          {element.atomic_mass}
+        </p>
+        <p>
+          <span>Phase of Matter: </span>
+          {element.phase}
+        </p>
+        <p>
+          <span>Summary: </span>
+          {element.summary}
+        </p>
+        <p>
+          <span>Symbol: </span>
+          {element.symbol}
+        </p>
+        <p>
+          <span>Electronic Configuration: </span>
+          {element.electron_configuration}
+        </p>
+        <p className="block">
+          <span>Block: </span>
+          {element.block}
+        </p>
+        <p>
+          <span>More Info: </span>
+          <a href={element.source} target="_blank">
+            {element.source}
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/PeriodicTable/PeriodicTable.jsx
+++ src/PeriodicTable/PeriodicTable.jsx
@@ -3,55 +3,60 @@ import data from "../PeriodicTableJSON.json";
 import "./PeriodicTable.css";
 import ElementModal from "../ElementModal/ElementModal";
 
-const colorMap = {
-    "noble gas": "#9328fe",
-    "unknown, predicted to be noble gas": "#9328fe",
-    "alkaline earth metal": "#ffa589",
-    "diatomic nonmetal": "#487bde",
-    "polyatomic nonmetal": "#2d6e96",
-    "alkali metal": "#ff4343",
-    "unknown, but predicted to be an alkali metal": "#ff4343",
-    "transition metal": "#ff8a00",
-    "unknown, probably transition metal": "#ff8a00",
-    "post-transition metal": "#31b870",
-    "unknown, probably post-transition metal": "#31b870",
-    "lanthanide": "#699797",
-    "actinide": "#976995",
-    "metalloid": "#1b99c1",
-    "unknown, probably metalloid": "#1b99c1",
+export const colorMap = {
+  "noble gas": "#9328fe",
+  "unknown, predicted to be noble gas": "#9328fe",
+  "alkaline earth metal": "#ffa589",
+  "diatomic nonmetal": "#487bde",
+  "polyatomic nonmetal": "#2d6e96",
+  "alkali metal": "#ff4343",
+  "unknown, but predicted to be an alkali metal": "#ff4343",
+  "transition metal": "#ff8a00",
+  "unknown, probably transition metal": "#ff8a00",
+  "post-transition metal": "#31b870",
+  "unknown, probably post-transition metal": "#31b870",
+  lanthanide: "#699797",
+  actinide: "#976995",
+  metalloid: "#1b99c1",
+  "unknown, probably metalloid": "#1b99c1",
 };
 
 export default function PeriodicTable() {
-
   const [selectedElement, setSelectedElement] = useState(null);
 
   function handleClick(element) {
     setSelectedElement(element);
-  };
+  }
 
   function closeModal() {
     setSelectedElement(null);
-  };
+  }
 
   return (
     <div className="periodic-chart">
-        <div className="periodic-table">
-          {data.elements.map((element) => (
-            <div
-              className={`elements ${element.name}`}
-              key={element.name}
-              style={{ gridColumn: element.xpos, gridRow: element.ypos, borderColor: colorMap[element.category], backgroundColor: colorMap[element.category], }}
-              onClick={() => handleClick(element)}
-            >
-              <strong className="symbol">{element.symbol}</strong>
-              <small className="number">{element.number}</small>
-              <small className="name">{element.name}</small>
-            </div>
-          ))}
-        </div>
-        {selectedElement &&
-            <ElementModal element={selectedElement} onClose={closeModal}/>
-          }
+      <div className="periodic-table">
+        {data.elements.map((element) => (
+          <div
+            className={`elements ${element.name}`}
+            key={element.name}
+            style={{
+              gridColumn: element.xpos,
+              gridRow: element.ypos,
+              borderColor: colorMap[element.category],
+              backgroundColor: colorMap[element.category],
+              cursor: "pointer",
+            }}
+            onClick={() => handleClick(element)}
+          >
+            <strong className="symbol">{element.symbol}</strong>
+            <small className="number">{element.number}</small>
+            <small className="name">{element.name}</small>
+          </div>
+        ))}
+      </div>
+      {selectedElement && (
+        <ElementModal element={selectedElement} onClose={closeModal} />
+      )}
     </div>
   );
 }
