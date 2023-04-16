24f9536817ef8966f7c21eeba443c92aca7b7fb5
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2023-04-16 22:02:01 +0100

    updated dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css'
 import PeriodicTable from './PeriodicTable/PeriodicTable'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import sun from './assets/sun-high.svg'
 import moon from './assets/moon.svg'
 
 function App() {
-  const [dark, setDark] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedDarkMode = localStorage.getItem('darkMode');
+    return savedDarkMode ? JSON.parse(savedDarkMode) : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode])
 
   return (
-    <div className={dark ? "app dark" : "app"}>
+    <div className={darkMode ? "app dark" : "app"}>
       <h1 className='h1'>THE PERIODIC TABLE OF ELEMENTS</h1>
-      <span onClick={() => setDark(!dark)}>
-        <img src={dark ? sun : moon} alt="light/dark mode" />
+      <span className={darkMode ? "light-icon" : "dark-icon"} onClick={() => setDarkMode(!darkMode)}>
+        <img src={darkMode ? sun : moon} alt="light/dark mode" />
       </span>
       <PeriodicTable />
       <footer>&#169; <a href="https://twitter.com/ehis_eleazar" target="_blank">Ehidiamhen</a> {new Date().getFullYear()}.</footer>
